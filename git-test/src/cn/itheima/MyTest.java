fc34131eb8b52b6863be375811806c8c1b845f5f
baohua <baohua@126.com>
2019-06-10 14:53:31 +0800

    添加一个方法

--- git-test/src/cn/itheima/MyTest.java
+++ git-test/src/cn/itheima/MyTest.java
@@ -4,4 +4,8 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("我的第一个java中的git");
     }
+
+    public void addTest(){
+        System.out.println("添加");
+    }
 }
